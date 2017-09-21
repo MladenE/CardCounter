2b88327d2b98b8c6f19b2b342a2379c742a9e58d
MladenE <mladen.edwards@outlook.com>
2017-09-21 18:01:07 +0100

    Add to Controller NameSpace

--- controllers/houseCard.js
+++ controllers/houseCard.js
@@ -1,38 +1,46 @@
 
-function HouseCard(newCard, houseHand) {
+var HouseCard = (function (ControllerNameSpace) {
     
-    GameState.update_Count(newCard);
-    GameState.update_Remaining_Cards_In_Shoe(newCard);
+    var _play = function (newCard, houseHand) {
 
-// loop through the players hands
-// - check if split hand exists before doing logic
-    
-    var house_hand_value = Hand.value_of_hand(houseHand);
-    // ! When playing the house cards I don't know whether to use the player's primary or split hand.
-    var player_hand_value = Hand.value_of_hand(models.hand.player);
+        GameState.update_Count(newCard);
+        GameState.update_Remaining_Cards_In_Shoe(newCard);
 
-    // Create clean new dto
-    var dto = Object.create(models.dto);
+        // loop through the players hands
+        // - check if split hand exists before doing logic
+        
+        var house_hand_value = Hand.value_of_hand(houseHand);
+        // ! When playing the house cards I don't know whether to use the player's primary or split hand.
+        var player_hand_value = Hand.value_of_hand(models.hand.player);
 
-    if (house_hand_value < player_hand_value) {
-        if (house_hand_value < 17) { // replace 17 with emum for "house stand limit"
-            // Do nothing. House will take another card.
-            dto.action = Emums.actions.doNothing;
-            dto.message = "House's hand has not reached the stand limit of 17";
-        } else if (house_hand_value = player_hand_value) {
-            dto.outcome = Enums.outcome.draw;
-        } else {
+        // Create clean new dto
+        var dto = Object.create(models.dto);
+
+        if (house_hand_value < player_hand_value) {
+            if (house_hand_value < 17) { // replace 17 with emum for "house stand limit"
+                // Do nothing. House will take another card.
+                dto.action = Emums.actions.doNothing;
+                dto.message = "House's hand has not reached the stand limit of 17";
+            } else if (house_hand_value = player_hand_value) {
+                dto.outcome = Enums.outcome.draw;
+            } else {
+                dto.outcome = Enums.outcome.win;
+                dto.message = "Player wins " + player_hand_value + " to " + house_hand_value;
+            }
+        } else if (Hand.is_bust(houseHand)) {
             dto.outcome = Enums.outcome.win;
-            dto.message = "Player wins " + player_hand_value + " to " + house_hand_value;
+            dto.message = "House is bust.";
+        } else {
+            dto.outcome = Enums.outcome.lose;
+            dto.message = "House wins " + house_hand_value + " to " + player_hand_value;
         }
-    } else if (Hand.is_bust(houseHand)) {
-        dto.outcome = Enums.outcome.win;
-        dto.message = "House is bust.";
-    } else {
-        dto.outcome = Enums.outcome.lose;
-        dto.message = "House wins " + house_hand_value + " to " + player_hand_value;
-    }
-    // send dto to UI
-    return dto;
+        // send dto to UI
+        return dto;
+    };
 
-}
+    var public = {};
+        public.play = _play;
+
+    ControllerNameSpace.HouseCard = public;
+    return ControllerNameSpace;
+});
--- controllers/playerCard.js
+++ controllers/playerCard.js
@@ -1,47 +1,58 @@
 
-function PlayerCard(newCard, playerHand, houseHand){
+var PlayerCard = (function (ControllerNameSpace) {
 
-    GameState.update_Count(newCard);
-    GameState.update_Remaining_Cards_In_Shoe(newCard);
+    var _play = function (newCard, playerHand, houseHand) {
+            
+        GameState.update_Count(newCard);
+        GameState.update_Remaining_Cards_In_Shoe(newCard);
 
-    // Create a clean new dto
-    var dto = Object.create(models.dto);
+        // Create a clean new dto
+        var dto = Object.create(models.dto);
 
-    if (playerHand.cards.length >= 2) {        
-        if (Hand.value_of_hand(playerHand) == 21) {
+        if (playerHand.cards.length >= 2) {        
+            if (Hand.value_of_hand(playerHand) == 21) {
 
-            dto.action = Enums.actions.stand;
-            dto.message = "Wait for house card.";
+                dto.action = Enums.actions.stand;
+                dto.message = "Wait for house card.";
 
-        } else if (Hand.is_bust(playerHand)) {
+            } else if (Hand.is_bust(playerHand)) {
 
-            dto.outcome = Enums.outcome.lose;
-            dto.message = "Bust";
+                dto.outcome = Enums.outcome.lose;
+                dto.message = "Bust";
 
-        } else {
+            } else {
 
-            var splitAction = Hand.has_pair(playerHand) ? Actions.get_split(playerHand, houseHand) : undefined;
+                var splitAction = Hand.has_pair(playerHand) ? Actions.get_split(playerHand, houseHand) : undefined;
 
-            if (splitAction != undefined) {
+                if (splitAction != undefined) {
 
-                dto.action = splitAction;
+                    dto.action = splitAction;
 
-            } else {             
+                } else {             
 
-                var basicForm = Hand.is_soft(playerHand) ? Actions.get_Soft(playerHand, houseHand) : Actions.get_Hard(playerHand, houseHand);
-                                    
-                var i20 = Actions.get_I20(playerHand, houseHand);
+                    var basicForm = Hand.is_soft(playerHand) ? Actions.get_Soft(playerHand, houseHand) : Actions.get_Hard(playerHand, houseHand);
+                                        
+                    var i20 = Actions.get_I20(playerHand, houseHand);
 
-                var extendedForm = i20 == false ? Actions.get_F4(playerHand, houseHand) : i20;
+                    var extendedForm = i20 == false ? Actions.get_F4(playerHand, houseHand) : i20;
 
-                var action = extendedForm == false ? basicForm : extendedForm;
+                    var action = extendedForm == false ? basicForm : extendedForm;
 
-                dto.action = action;
+                    dto.action = action;
+                }
             }
+        } else {
+            dto.message = "2 or more cards are required.";
         }
-    } else {
-        dto.message = "2 or more cards are required.";
-    }
-    // send dto to UI
-    return dto;
-}
+        // send dto to UI
+        return dto;
+    
+    };
+
+    var public = {};
+        public.play = _play;
+
+    ControllerNameSpace.HouseCard = public;
+    return ControllerNameSpace;
+
+});
