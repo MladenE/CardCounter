8c2e0ef4404e5ffd9f50e91305eefc6f2381da2b
MladenE <mladen.edwards@outlook.com>
2017-09-27 18:57:06 +0100

    Move _card object to Collection

--- domain/models.js
+++ domain/models.js
@@ -17,23 +17,6 @@ var Models = (function (DomainNameSpace) {
             }
         };
 
-    // What is this. It isn't the shoe! Should the countValues all be 0?
-    var _card = {
-            _a:  { name: enums.cardNames.ace,   value: enums.aceValue.hard, countValue: 0 },
-            _2:  { name: enums.cardNames.two,   value: 2, countValue: 0 },
-            _3:  { name: enums.cardNames.three, value: 3, countValue: 0 },
-            _4:  { name: enums.cardNames.four,  value: 4, countValue: 0 },
-            _5:  { name: enums.cardNames.five,  value: 5, countValue: 0 },
-            _6:  { name: enums.cardNames.six,   value: 6, countValue: 0 },
-            _7:  { name: enums.cardNames.seven, value: 7, countValue: 0 },
-            _8:  { name: enums.cardNames.eight, value: 8, countValue: 0 },
-            _9:  { name: enums.cardNames.nine,  value: 9, countValue: 0 },
-            _10: { name: enums.cardNames.ten,   value: 10, countValue: 0 },
-            _J:  { name: enums.cardNames.jack,  value: 10, countValue: 0 },
-            _Q:  { name: enums.cardNames.queen, value: 10, countValue: 0 },
-            _K:  { name: enums.cardNames.king,  value: 10, countValue: 0 }
-        };
-
     // Object to pass back from the controllers
     var _dto = {
             outcome: undefined, // WIN | LOSE | DRAW | undefined
@@ -44,7 +27,6 @@ var Models = (function (DomainNameSpace) {
     // Public methods (revealer pattern)
     var public = {};
         public.hand = _hand;
-        public.card = _card;
         public.dto  = _dto;
 
     // Add methods to the namespace
