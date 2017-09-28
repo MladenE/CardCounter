dd4248ede0079b68da6ac09f19e922be84388d7b
MladenE <mladen.edwards@outlook.com>
2017-09-28 17:20:13 +0100

    Initializes the js for the event handlers

---
+++ main/startup.js
@@ -0,0 +1,47 @@
+
+// Rename this to startApp.js
+// Acts like Main()
+
+var Main = (function (ConfigFilePath) {
+
+    var _config;
+
+    var _parseConfigFile = function () {
+        // Check file exists first using Validations();
+        _config = JSON.parse(ConfigFilePath);
+    };
+
+    var _namespaces = function () {
+        var Persistance = {}
+            CardHistory(Persistance);
+            Shoe(Persistance);
+        
+        var Domain = {};
+            Eums(Domain);
+            Collection(Domain);
+            Models(Domain);
+        
+        var Services = {};
+            Actions(Services);
+            GameState(Services);
+            Hand(Services);
+        
+        var Controllers = {};
+            HouseCard(Controllers);
+            PlayerCard(Controllers);
+    };
+
+    var _init = function () {
+        _parseConfigFile();
+        _namespaces();
+    };
+
+    _init();
+
+})(ConfigFilePath);
+
+// Namespaces to attach modules to.
+// Order is important!
+
+
+
