15a54365d041147b573239b436a5cd625c883979
MladenE <mladen.edwards@outlook.com>
2017-09-28 17:20:41 +0100

    Validation fro config.json

---
+++ services/validation.js
@@ -0,0 +1,6 @@
+var Validation = (function () {
+    // validations for checking config.json values
+    var _string = function (input){};
+    var _int = function (input){};
+    var _url = function (input){};
+});
