b2ebbddbe022ec7bd57f5b608ad243a7fe5354dd
MladenE <mladen.edwards@outlook.com>
2017-09-28 17:19:29 +0100

    Controlls for NewGame, Reset, etc.

---
+++ controllers/game.js
@@ -0,0 +1,19 @@
+var Game = (function (ControllerNameSpace) {
+
+    var _newGame = function () {
+        // 
+        // clear hands
+    };
+
+    var _reset = function () {
+        // clear everything: hands, shoe, cardHistory
+    };
+
+    var public = {};
+    public.newGame   = _newGame;
+    //public.undoEvent = _undoEvent; // removes the last action. Gets data from cardHistory {hand, card} and reversed it.
+    public.reset     = _reset;
+
+    ControllerNameSpace.HouseCard = public;
+    return ControllerNameSpace;
+});
