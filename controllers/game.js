807089af6089e643e50f54b8ead40464f7437f66
MladenE <mladen.edwards@outlook.com>
2017-10-10 17:31:36 +0100

    Reset hand

--- controllers/game.js
+++ controllers/game.js
@@ -3,10 +3,15 @@ var Game = (function (ControllerNameSpace) {
     var _newGame = function () {
         // 
         // clear hands
+        Domain.Models.hand.house.cards = [];
+        Domain.Models.hand.player.primary.cards = [];
+        Domain.Models.hand.player.split.cards = [];
     };
 
     var _reset = function () {
         // clear everything: hands, shoe, cardHistory
+        _newGame();
+        Persistance.Shoe.Init(Config.gameRules.numberOfDecks);
     };
 
     var public = {};
